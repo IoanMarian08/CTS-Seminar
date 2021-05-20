9802e407dbdbfd1b95e0791db708658d3cceac0c
Ioan-Marian <ioanmarianmarghioala8@gmail.com>
2021-05-20 18:56:23 +0300

    JUnit Tests - Seminar 12

--- Seminar 12/src/ro/ase/cts/teste/StudentTests.java
+++ Seminar 12/src/ro/ase/cts/teste/StudentTests.java
@@ -22,8 +22,8 @@ public class StudentTests {
 	public void testConstructorFaraParametrii() {
 		Student student = new Student(); //apelez constr
 		
-		assertNotNull(student.getNote());
-		assertNotNull(student.getNume());
+		assertNotNull("Atributul note nu a fost initializat",student.getNote());
+		assertNotNull("Atributul numen nu a fost initializat",student.getNume());
 	}
 	
 	
@@ -101,30 +101,48 @@ public class StudentTests {
 	}
 	
 	
+	@Test  // pentru junit 3
+	public void testGetNotaExceptie() {
+		Student student = new Student();
+		
+		student.adaugaNota(5);
+		student.adaugaNota(7);
+		// 1 - se executa
+		try {
+			//2 - se executa
+			student.getNota(5); // va arunca oe xceptie ca am dat doar o nota
+			//3  -- nu se va executa ca a esuat asta de sus
+			fail("Nu trebuia sa ajunga aici. Metoda nu arunca exceptie");
+		}catch (IndexOutOfBoundsException exceptie) {
+			//4 -- se va executa
+		} 
+		catch (Exception e) {
+			fail("Metoda arunca o alta exceptie");
+		}
+		//5 -- se va executa
+	}
 	
 	
+	@Test(expected = IndexOutOfBoundsException.class)  // JUNIT 4
+	public void testGetNotaExceptieJUNIT4() {
+		Student student = new Student();
+		
+		student.adaugaNota(5);
+		student.adaugaNota(7);
+		student.getNota(5);
+	}
 	
+	//JUNIT 5
+	@Test
+	public void testGetNotaJUNIT5 () {
+		Student student = new Student();
+		
+		student.adaugaNota(8);
+		student.adaugaNota(7);
+		
+		assertThrows(IndexOutOfBoundsException.class,() -> {
+			student.getNota(5);
+		});
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
